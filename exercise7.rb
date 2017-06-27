a2ab4c5c7319ea1d321d380278d72ab65a51d04f
maryfranks <maryfranks@gmail.com>
2017-06-27 14:45:12 -0400

    solved exercise 7 again changing method

--- exercise7.rb
+++ exercise7.rb
@@ -2,7 +2,7 @@ def wrap_text(text, symbol)
   symbol + text + symbol
 end
 
-wrap_text("Hello", "+++")
+puts wrap_text("Hello", "+++")
 
 
 first_message = wrap_text("new message", "\#\#\#")
@@ -12,3 +12,9 @@ second_message = wrap_text(first_message, "+++")
 final_message = wrap_text(second_message, "---")
 
 puts "This is my #{final_message}"
+
+def wrap_text_redux(text_b, symbol_b)
+  symbol_b + text_b + symbol_b.reverse
+end
+
+puts wrap_text_redux("Wooo", "---+++===")
