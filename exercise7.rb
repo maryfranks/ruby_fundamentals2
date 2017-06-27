3fed392776c881207b30366eed607e7aacee158c
maryfranks <maryfranks@gmail.com>
2017-06-27 14:09:25 -0400

    finished exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -1,6 +1,14 @@
 def wrap_text(text, symbol)
-  result = puts "#{symbol}#{text}#{symbol}"
-  return result
+  symbol + text + symbol
 end
 
 wrap_text("Hello", "+++")
+
+
+first_message = wrap_text("new message", "\#\#\#")
+
+second_message = wrap_text(first_message, "+++")
+
+final_message = wrap_text(second_message, "---")
+
+puts "This is my #{final_message}"
