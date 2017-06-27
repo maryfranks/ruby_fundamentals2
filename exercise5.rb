048db20a1b4b1459363a45bb02ad1479af7d0a9b
maryfranks <maryfranks@gmail.com>
2017-06-27 13:26:28 -0400

    finished exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,10 @@
+def greet_backwards(name)
+  name.reverse!
+  puts "Hello #{name}!  Welcome Home."
+end
+
+greet_backwards("Amanda")
+greet_backwards("Bob")
+greet_backwards("Shirley")
+greet_backwards("Sue")
+greet_backwards("Andy")
