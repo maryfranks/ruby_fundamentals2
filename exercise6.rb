d009670eaa3c712569d9920d7138d630d115d2a9
maryfranks <maryfranks@gmail.com>
2017-06-27 14:40:51 -0400

    updated exercise 6 to fix rounding error

--- exercise6.rb
+++ exercise6.rb
@@ -2,7 +2,7 @@ puts "What is the temperature in Fahrenheit?"
 temperature_in_fahrenheit = gets.to_i
 
 def convert_temp_to_celsius(f)
-  (f - 32) * 5/9
+  (f - 32) * 5.0/9
 end
 
 puts "The temperature outside is #{convert_temp_to_celsius(temperature_in_fahrenheit)} degrees Celsius."
