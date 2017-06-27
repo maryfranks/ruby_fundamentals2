7e5cb442da64cb751bcb2cc25f7a8e8a0a971341
maryfranks <maryfranks@gmail.com>
2017-06-27 13:33:21 -0400

    finished exercise 6

---
+++ exercise6.rb
@@ -0,0 +1,8 @@
+puts "What is the temperature in Fahrenheit?"
+temperature_in_fahrenheit = gets.to_i
+
+def convert_temp_to_celsius(f)
+  (f - 32) * 5/9
+end
+
+puts "The temperature outside is #{convert_temp_to_celsius(temperature_in_fahrenheit)} degrees Celsius."
