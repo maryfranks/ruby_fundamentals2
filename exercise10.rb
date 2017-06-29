fa05b451f1a18bdd8e46e24543438105932d020f
maryfranks <maryfranks@gmail.com>
2017-06-29 15:40:04 -0400

    exercise 10 working with hashes

---
+++ exercise10.rb
@@ -0,0 +1,15 @@
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+students[:cohort4] = 43
+
+
+
+# students.each do |key, value|
+#   puts "#{key}: #{value} students"
+# end
+
+# p students.keys
