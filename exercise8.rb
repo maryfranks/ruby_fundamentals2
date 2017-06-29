c232c827d390c4d7dc46afae96c7de68415e94f8
maryfranks <maryfranks@gmail.com>
2017-06-29 15:18:14 -0400

    completed exercise with method

--- exercise8.rb
+++ exercise8.rb
@@ -6,3 +6,15 @@ my_expenses.each do |expense|
 end
 p sum
 # puts "The answer should be #{50 + 45 + 150 + 30 + 60}"
+
+second_expenses = [10, 20, 30, 40]
+
+def add_expenses(array_name)
+  sum = 0
+  array_name.each do |expense|
+    sum = sum + expense
+  end
+  p sum
+end
+add_expenses(my_expenses)
+add_expenses(second_expenses)
