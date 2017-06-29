5bc445cb1dc9420e52fe24f79c070f3ca7299503
maryfranks <maryfranks@gmail.com>
2017-06-29 16:20:38 -0400

    finished exercise 11

--- exercise11.rb
+++ exercise11.rb
@@ -3,15 +3,20 @@ my_dogs = [
   { :name => 'Cindy', :position => 8 },
   { :name => 'Jade', :position => 11 },
 ]
-
+neighbor_dogs = [
+  {:name => "Mike", :position => 4},
+  {:name => "Sam", :position => 12},
+  {:name => "Joe", :position => 14}
+]
 # p my_dogs
 
-def get_absent_dogs(array_of_dogs)
+def call_absent_dogs(array_of_dogs)
   array_of_dogs.each do |hash|
     if hash[:position] > 10
-      puts "#{hash[:name]} is out of the yard"
+      puts "Come here, #{hash[:name]}!"
     end
   end
 end
 
-p get_absent_dogs(my_dogs)
+p call_absent_dogs(my_dogs)
+p call_absent_dogs(neighbor_dogs)
